a5bdf3876220d2382176aed0ac55988ae592070b
aditi <adishrestha2005@gmail.com>
2025-12-04 17:53:15 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/aditi/sliceofheaven/slice_of_heaven/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aditi.sliceofheaven.slice_of_heaven
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:slice_of_heaven/app.dart';
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(App());
+}
+
