e56e69780f1b0a4a2986bccc7fa04ae57571faf1
aditi <adishrestha2005@gmail.com>
2025-12-05 18:09:41 +0545

    login page

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // Call the navigation function immediately
+ 
     _navigateToNextScreen();
   }
 
