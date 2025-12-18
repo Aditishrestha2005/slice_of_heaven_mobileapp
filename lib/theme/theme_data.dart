ff6c1a73c9ea20169061778ad5e57b9e60c31a42
aditi <adishrestha2005@gmail.com>
2025-12-18 21:52:52 +0545

    theme data

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -1,23 +1,21 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class AppTheme {
-//   static ThemeData lightTheme() {
-//     return ThemeData(
-//       colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
-//       useMaterial3: true,
-//       fontFamily: 'OpenSansItalic',
-//       elevatedButtonTheme: ElevatedButtonThemeData(
-//         style: ElevatedButton.styleFrom(
-//           textStyle: const TextStyle(
-//             fontSize: 18,
-//             fontFamily: 'OpenSansBold',
-//           ),
-//           backgroundColor: Colors.lightGreen,
-//           shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.circular(5),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class AppTheme {
+  static ThemeData lightTheme() {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+      useMaterial3: true,
+      fontFamily: 'OpenSansItalic',
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          textStyle: const TextStyle(
+            fontSize: 18,
+            fontFamily: 'OpenSansBold',
+          ),
+          backgroundColor: Colors.lightGreen,
+        
+        ),
+      ),
+    );
+  }
+}
