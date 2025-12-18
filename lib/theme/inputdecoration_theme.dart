83d33890cc75647dda842ecd8a9871cdb472440f
aditi <adishrestha2005@gmail.com>
2025-12-18 21:49:50 +0545

    input decoration

--- lib/theme/inputdecoration_theme.dart
+++ lib/theme/inputdecoration_theme.dart
@@ -1,21 +1,21 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// InputDecorationTheme getinputdecorationtheme(){
-//   return InputDecorationTheme(
-//     filled: true,
-//     fillColor: Colors.grey.shade200,
-//     border: OutlineInputBorder(
-//       borderRadius: BorderRadius.circular(10),
-//       borderSide: const BorderSide(color: Colors.green),
-//     ),
-//     focusedBorder: OutlineInputBorder(
-//       borderRadius: BorderRadius.circular(10),
-//       borderSide: const BorderSide(color: Colors.green, width: 2),
-//     ),
-//     labelStyle: const TextStyle(
-//       fontFamily: 'OpenSans-Regular',
-//       fontSize: 14,
-//       color: Colors.black87,
-//     ),
-//   );
-// }
+InputDecorationTheme getinputdecorationtheme(){
+  return InputDecorationTheme(
+    filled: true,
+    fillColor: Colors.grey.shade200,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: const BorderSide(color: Colors.green),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: const BorderSide(color: Colors.green, width: 2),
+    ),
+    labelStyle: const TextStyle(
+      fontFamily: 'OpenSans-Regular',
+      fontSize: 14,
+      color: Colors.black87,
+    ),
+  );
+}
