7b1562cad139c9d81b315cb49a8ab9d79a5c69ea
aditi <adishrestha2005@gmail.com>
2026-01-03 10:51:52 +0545

    log out usecase

--- lib/features/auth/domain/usecases/getuser_byemail.dart
+++ lib/features/auth/domain/usecases/getuser_byemail.dart
@@ -1,20 +1,20 @@
 
-import 'package:dartz/dartz.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:slice_of_heaven/core/error/failure.dart';
-import 'package:slice_of_heaven/features/auth/data/repositories/auth_repository.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:slice_of_heaven/core/error/failure.dart';
+// import 'package:slice_of_heaven/features/auth/data/repositories/auth_repository.dart';
 
-import '../entities/auth_entity.dart';
-import '../repositories/auth_repository.dart';
+// import '../entities/auth_entity.dart';
+// import '../repositories/auth_repository.dart';
 
-final getUserByEmailProvider = Provider(
-  (ref) => GetUserByEmailUsecase(ref.read(authRepositoryProvider)),
-);
+// final getUserByEmailProvider = Provider(
+//   (ref) => GetUserByEmailUsecase(ref.read(authRepositoryProvider)),
+// );
 
-class GetUserByEmailUsecase {
-  final IAuthRepository repository;
-  GetUserByEmailUsecase(this.repository);
-  Future<Either<Failure, AuthEntity>> call(String email) {
-    return repository.getUserByEmail(email);
-  }
-}
+// class GetUserByEmailUsecase {
+//   final IAuthRepository repository;
+//   GetUserByEmailUsecase(this.repository);
+//   Future<Either<Failure, AuthEntity>> call(String email) {
+//     return repository.getUserByEmail(email);
+//   }
+// }
