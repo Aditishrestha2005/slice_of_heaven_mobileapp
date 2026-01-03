bd38969b388c1dc5a97175da304343f5e1a77c45
aditi <adishrestha2005@gmail.com>
2026-01-03 10:49:59 +0545

    Home Screen

--- lib/features/dashboard/bottom_screen/home_screen.dart
+++ lib/features/dashboard/bottom_screen/home_screen.dart
@@ -6,7 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
       body: const Center(
         child: Text(
           "This is home screen",
