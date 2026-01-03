8874c75248d836743eeaadd4d603bd31a7093ba1
aditi <adishrestha2005@gmail.com>
2026-01-03 11:01:43 +0545

    change

--- lib/app/theme/bottomnavigationbar_theme.dart
+++ lib/app/theme/bottomnavigationbar_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 BottomNavigationBarThemeData getBottomNavigationBarTheme() {
   return BottomNavigationBarThemeData(
-    backgroundColor: const Color.fromARGB(255, 198, 116, 9),
+    backgroundColor: const Color.fromARGB(255, 234, 203, 163),
     selectedItemColor: Colors.black,
     unselectedItemColor: Colors.white,
     selectedLabelStyle: const TextStyle(
