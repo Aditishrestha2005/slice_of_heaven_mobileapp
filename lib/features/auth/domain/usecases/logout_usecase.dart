4e349c0ce4bfc01f8ee439b0b8f3b0645f1b25bc
aditi <adishrestha2005@gmail.com>
2026-01-17 12:37:17 +0545

    usecase updated

--- lib/features/auth/domain/usecases/logout_usecase.dart
+++ lib/features/auth/domain/usecases/logout_usecase.dart
@@ -1,37 +1,37 @@
-import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:slice_of_heaven/core/error/failure.dart';
-import 'package:slice_of_heaven/core/usecase/app_usecase.dart';
-import 'package:slice_of_heaven/features/auth/data/repositories/auth_repository.dart';
-import 'package:slice_of_heaven/features/auth/domain/entities/auth_entity.dart';
-import 'package:slice_of_heaven/features/auth/domain/repositories/auth_repository.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:equatable/equatable.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:slice_of_heaven/core/error/failure.dart';
+// import 'package:slice_of_heaven/core/usecase/app_usecase.dart';
+// import 'package:slice_of_heaven/features/auth/data/repositories/auth_repository.dart';
+// import 'package:slice_of_heaven/features/auth/domain/entities/auth_entity.dart';
+// import 'package:slice_of_heaven/features/auth/domain/repositories/auth_repository.dart';
 
 
 
-class LoginParams extends Equatable {
-  final String email;
-  final String password;
+// class LoginParams extends Equatable {
+//   final String email;
+//   final String password;
 
-  const LoginParams({required this.email, required this.password});
+//   const LoginParams({required this.email, required this.password});
 
-  @override
-  List<Object?> get props => [email, password];
-}
+//   @override
+//   List<Object?> get props => [email, password];
+// }
 
-final loginUseCaseProvider = Provider<LoginUseCase>((ref) {
-  final authRepository = ref.read(authRepositoryProvider);
-  return LoginUseCase(authRepository: authRepository);
-});
+// final loginUseCaseProvider = Provider<LoginUseCase>((ref) {
+//   final authRepository = ref.read(authRepositoryProvider);
+//   return LoginUseCase(authRepository: authRepository);
+// });
 
-class LoginUseCase implements UsecaseWithParams<AuthEntity, LoginParams> {
-  final IAuthRepository _authRepository;
+// class LoginUseCase implements UsecaseWithParams<AuthEntity, LoginParams> {
+//   final IAuthRepository _authRepository;
 
-  LoginUseCase({required IAuthRepository authRepository})
-    : _authRepository = authRepository;
+//   LoginUseCase({required IAuthRepository authRepository})
+//     : _authRepository = authRepository;
 
-  @override
-  Future<Either<Failure, AuthEntity>> call(LoginParams params) {
-    return _authRepository.login(params.email, params.password);
-  }
-}
+//   @override
+//   Future<Either<Failure, AuthEntity>> call(LoginParams params) {
+//     return _authRepository.login(params.email, params.password);
+//   }
+// }
